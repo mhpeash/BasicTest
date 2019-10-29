25fdec819ee5db0c9834e9a2116b3a8b2a8d6a37
Mohammad Mohibul Hasan <mhpeash.ewu@hotmail.com>
2019-10-30 01:22:45 +0600

    modefied video player design

--- app/src/main/java/com/shurjomukhi/basictest/Q3/VideoPlayer.java
+++ app/src/main/java/com/shurjomukhi/basictest/Q3/VideoPlayer.java
@@ -24,6 +24,7 @@ public class VideoPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        getSupportActionBar().hide();
 
         init();
         play();
